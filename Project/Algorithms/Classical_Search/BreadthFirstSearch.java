77ad24c886e7d671999ab093e92081b929340145
Phillips <75767057+phillipscg22@users.noreply.github.com>
2021-05-04 10:24:01 -0400

    Add files via upload

---
+++ Project/Algorithms/Classical_Search/BreadthFirstSearch.java
@@ -0,0 +1,110 @@
+package Project.Algorithms.Classical_Search;
+
+import Project.Game_Engine.Environment;
+import Project.Infrastructure.Node;
+import Project.Game_Engine.Percepts;
+import Project.Infrastructure.SuccessorFunction;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch implements Percepts {
+	
+    private Node rootNode;
+    private ArrayList<Node> nodesDB;  // HashSet
+    //SuccessorFunction succFunction;
+    private Queue<Node> fringe;
+    private LinkedList<Node> expandedNodes;
+    private Environment environment;
+
+    public BreadthFirstSearch(Environment environment) {
+
+        fringe = new LinkedList<>();
+        expandedNodes = new LinkedList<>();
+        
+        nodesDB = new ArrayList<>();
+        this.environment = environment;
+    }
+    
+    public boolean isDuplicateNode(Node thatNode) {
+
+
+        //search DB
+        for (Node thisNode : nodesDB) {
+//            System.out.println(thisNode.getState().getLocation().getX() + " : " + thisNode.getState().getLocation().getY());
+//            System.out.println(thatNode.getState().getLocation().getX() + " : " + thatNode.getState().getLocation().getY());
+            if(thisNode.equals(thatNode)) {
+                System.out.println("duplicate in method");
+                return true;
+            }
+        }
+
+//        if(nodesDB.contains(thatNode))
+//            return true;
+
+        nodesDB.add(thatNode);
+
+        return false;
+     }
+
+    public Node search(Node rootNode) { //  (String [][] grid, Node rootNode)  {
+
+        this.rootNode = rootNode;
+
+        if(rootNode.getState().getPercept().equals(GOAL_CELL))
+            return rootNode;
+
+        // add the root node to the fringe
+        fringe.add(rootNode);
+       // System.out.println(fringe.size());
+
+        while(!fringe.isEmpty()) {
+
+            //System.out.println(fringe.size() + " fringe size");
+            Node currentNode = fringe.remove();
+
+            if(!isDuplicateNode(currentNode)) {
+                expandedNodes = SuccessorFunction.expand(currentNode, environment);
+                System.out.println("expanded size: " + expandedNodes.size());
+                //System.out.println("not duplicate");
+                //System.out.println(currentNode.getState().getLocation().getX() + " : " + currentNode.getState().getLocation().getY());
+            }
+            else {
+                System.out.println("duplicate");
+            }
+
+            while(!expandedNodes.isEmpty()) {
+                Node tempNode = expandedNodes.remove();
+
+                //Check if it is a goal cell
+                //System.out.println(expandedNodes.size());
+//                if(currentNode.getState().getPercept() == "null") {System.out.println("percept null");}
+//                System.out.println("current percept: " + currentNode.getState().getPercept());
+
+                if(currentNode == null || currentNode.getState() == null) {
+               //     System.out.println("current node is null");
+                    continue;
+                }
+                else if(currentNode.getState().getPercept().equals(GOAL_CELL)) {
+                    System.out.println("Location : " + currentNode.getState().getLocation().getX() + " : " + currentNode.getState().getLocation().getY());
+                    System.out.println("goal" + currentNode.getState().getLocation().getX() + " : " + currentNode.getState().getLocation().getY());
+                    return currentNode;
+                }
+                else {
+                    System.out.println(currentNode.getState().getPercept());
+                    System.out.println("Location : " + currentNode.getState().getLocation().getX() + " : " + currentNode.getState().getLocation().getY());
+                }
+
+                System.out.println("Direction of currentNode is: " + currentNode.getState().getAgent().getDirection());
+                fringe.add(currentNode);
+            }
+         //  System.out.println("out of inner");
+
+         //   System.out.println(fringe.size());
+        }
+
+        return null;
+    }
+}
---
+++ Project/Game_Engine/TruckAgent.java
@@ -0,0 +1,95 @@
+package Project.Game_Engine;
+
+import java.util.UUID;
+import javafx.scene.Group;
+import javafx.scene.transform.Rotate;
+
+public class TruckAgent extends Group implements Constants {
+
+    private UUID agentId;
+    private boolean movable;
+    private int direction;
+    private Location location;
+
+    public TruckAgent() {
+
+        agentId = UUID.randomUUID();
+        movable = false;
+        direction = FACING_WEST;
+        location = new Location();
+    }
+
+    public TruckAgent(boolean movable, Location location) {
+
+        this.agentId = UUID.randomUUID();
+        this.movable = movable;
+        direction = FACING_WEST;
+        this.location = location;
+    }
+
+    //getter and setter for uuid
+    public void setUUID(UUID agentId) {
+        this.agentId = agentId;
+    }
+
+    public UUID getUUID() {
+        return agentId;
+    }
+
+
+
+    public void setMovable(boolean movable) {
+
+        this.movable = movable;
+    }
+
+    public boolean getMovable() {
+
+        return movable;
+    }
+
+    public void setDirection(int direction) {
+
+        this.direction = direction;
+    }
+
+    public int getDirection() {
+
+        return direction;
+    }
+
+    public void setLocation(Location location) {
+
+        this.location = location;
+    }
+
+    public Location getLocation() {
+
+        return location;
+    }
+
+    public void flipTruck() {
+
+        this.setRotationAxis(Rotate.Y_AXIS);
+        this.setRotate(180);
+        setDirection(FACING_EAST);
+    }
+
+    public TruckAgent refresh(double width, double height, Location location) {
+
+        return this.build(width, height, location);
+    }
+
+    public void clear() {
+
+        this.getChildren().clear();
+        this.setMovable(false);
+    }
+
+    //Test when moving to make sure it is the correct truck
+    @Override
+    public String toString() {
+
+        return "Agent ID: " + getUUID() + ", x = " + getLocation().getX() + ", y = " + getLocation().getY();
+    }
+}
