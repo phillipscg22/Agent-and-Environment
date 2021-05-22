1fed48a5361e7c8ab63733a68ca6075600b66f7b
Phillips <75767057+phillipscg22@users.noreply.github.com>
2021-05-22 15:46:12 -0400

    Add files via upload

---
+++ Project/Game_Engine/Location.java
@@ -0,0 +1,40 @@
+package Project.Game_Engine;
+
+public class Location {
+
+	private int x;
+	private int y;
+
+	public Location() {
+
+		this.x = 0;
+		this.y = 0;
+	}
+
+	public Location(int x, int y) {
+
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(int x) {
+
+		this.x = x;
+	}
+
+	public int getX() {
+
+		return  x;
+	}
+
+	public void setY(int y) {
+
+		this.y = y;
+	}
+
+	public int getY() {
+
+		return  y;
+	}
+
+}
