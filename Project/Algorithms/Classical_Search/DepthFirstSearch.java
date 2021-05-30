862dc94c05ac825a45d4fd5914e1e184c5aa2204
Phillips <75767057+phillipscg22@users.noreply.github.com>
2021-05-30 15:22:14 -0400

    Add files via upload

---
+++ Project/Algorithms/Classical_Search/DepthFirstSearch.java
@@ -0,0 +1,77 @@
+package Project.Algorithms.Classical_Search;
+
+import Project.Game_Engine.Environment;
+import Project.Infrastructure.Node;
+import Project.Game_Engine.Percepts;
+import Project.Infrastructure.SuccessorFunction;
+
+import java.util.*;
+
+public class DepthFirstSearch implements Percepts {
+
+    private Node rootNode;
+    private HashMap<String, Node> nodesDB;
+    private Stack<Node> fringe;
+    private LinkedList<Node> expandedNodes;
+    private Environment environment;
+
+    public DepthFirstSearch(Environment environment) {
+
+        fringe = new Stack<>();
+        expandedNodes = new LinkedList<>();
+
+        nodesDB = new HashMap<>();
+        this.environment = environment;
+    }
+
+    public boolean isDuplicateNode(Node thatNode) {
+
+        //search DB
+        if(nodesDB.containsKey(thatNode.toString())) {
+            nodesDB.put(thatNode.toString(), thatNode);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Node search(Node rootNode) throws CloneNotSupportedException {
+
+        this.rootNode = rootNode;
+
+        if(rootNode == null)
+            System.out.println("root node null");
+
+        if(rootNode.getState().getPercept().equals(GOAL_CELL))
+            return rootNode;
+
+        // add the root node to the fringe
+        fringe.push(rootNode);
+
+        do {
+            Node currentNode = fringe.pop();
+
+            if(!isDuplicateNode(currentNode)) {
+                expandedNodes = SuccessorFunction.expand(currentNode, environment);
+            }
+
+            while(!expandedNodes.isEmpty()) {
+                Node tempNode = expandedNodes.remove();
+
+                if(tempNode == null || tempNode.getState() == null) {
+                    continue;
+                }
+                else if(tempNode.getState().getPercept().equals(GOAL_CELL) && !environment.getCells()[tempNode.getState().getLocation().getX()][tempNode.getState().getLocation().getY()].getGoalFound()) {
+                    environment.getCells()[tempNode.getState().getLocation().getX()][tempNode.getState().getLocation().getY()].setGoalFound(true);
+                    return tempNode;
+                }
+
+                fringe.push(tempNode);
+            }
+
+        } while(!fringe.isEmpty());
+
+        return null;
+    }
+}
+
